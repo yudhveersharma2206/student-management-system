3dab4cd679e2544e51752614f2334ea1a1a74525
yudhveersharma2206 <sharmayudhveer085@gmail.com>
2026-04-25 23:20:22 +0530

    Fix component scan

--- backend/src/main/java/com/example/student_management/StudentManagementApplication.java
+++ backend/src/main/java/com/example/student_management/StudentManagementApplication.java
@@ -3,9 +3,8 @@ package com.example.student_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.student_management")
 public class StudentManagementApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
